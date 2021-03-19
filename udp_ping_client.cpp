9ca954b0ab673375dc1ac438ab233ca2f263ad12
Danielle O'Neal <doneal1@cs.binghamton.edu>
2021-03-19 17:57:28 -0400

    edit

--- udp_ping_client.cpp
+++ udp_ping_client.cpp
@@ -12,7 +12,23 @@
 
 
 int main() { 
+	int sockfd, n;
+	socklen_t len;
+	char buffer[1024];
+	struct sockaddr_in servaddr;
+
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	memset(&servaddr, 0, sizeof(servaddr)); 
+
+	servaddr.sin_family = AF_INET; // IPv4 
+	servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
+	servaddr.sin_port = htons(PORT); // port number
 
 
+	for(int i = 0; i < 10; i++){
+		
+
+	}
 
 }
